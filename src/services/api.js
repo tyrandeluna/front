637fff37b361afa03d7900abeaca007b973e87c0
Maria Beatriz de Almeida Batista <mb.almeidadv@hotmail.com>
2021-01-09 15:32:21 -0300

    fixing bug with api connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from './auth'
 
-const api = axios.create({ baseURL: DB_HOST })
+const api = axios.create({ baseURL: 'https://allocate-ufc-back.herokuapp.com' })
 
 api.interceptors.request.use(async config => {
     const token = getToken()
